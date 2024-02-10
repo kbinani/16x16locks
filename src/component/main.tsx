bb14433fc70a32f5746800ddd0cbb666ba5e14e9
kbinani <kbinani.bt@gmail.com>
2024-02-11 08:23:51 +0900

    Display characters in 3x3

--- src/component/main.tsx
+++ src/component/main.tsx
@@ -5,12 +5,39 @@ import { CharacterComponent } from "./character";
 
 export const Main: React.FC = () => {
   return (
-    <div style={{ display: "flex", flexDirection: "row" }}>
-      <div style={{ margin: 10 }}>
-        <CharacterComponent character={generate(kDefaultRandom)} size={20} />
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div style={{ display: "flex" }}>
+        <div style={{ margin: 10 }}>
+          <CharacterComponent character={generate(kDefaultRandom)} size={20} />
+        </div>
+        <div style={{ margin: 10 }}>
+          <CharacterComponent character={generate(kDefaultRandom)} size={20} />
+        </div>
+        <div style={{ margin: 10 }}>
+          <CharacterComponent character={generate(kDefaultRandom)} size={20} />
+        </div>
       </div>
-      <div style={{ margin: 10 }}>
-        <CharacterComponent character={generate(kDefaultRandom)} size={20} />
+      <div style={{ display: "flex" }}>
+        <div style={{ margin: 10 }}>
+          <CharacterComponent character={generate(kDefaultRandom)} size={20} />
+        </div>
+        <div style={{ margin: 10 }}>
+          <CharacterComponent character={generate(kDefaultRandom)} size={20} />
+        </div>
+        <div style={{ margin: 10 }}>
+          <CharacterComponent character={generate(kDefaultRandom)} size={20} />
+        </div>
+      </div>
+      <div style={{ display: "flex" }}>
+        <div style={{ margin: 10 }}>
+          <CharacterComponent character={generate(kDefaultRandom)} size={20} />
+        </div>
+        <div style={{ margin: 10 }}>
+          <CharacterComponent character={generate(kDefaultRandom)} size={20} />
+        </div>
+        <div style={{ margin: 10 }}>
+          <CharacterComponent character={generate(kDefaultRandom)} size={20} />
+        </div>
       </div>
     </div>
   );
