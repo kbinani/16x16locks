7439d7e86c614a8675f6a14228f6e0b1e9461a7d
kbinani <kbinani.bt@gmail.com>
2024-02-11 16:18:57 +0900

    Seed random generator by user input

--- src/component/main.tsx
+++ src/component/main.tsx
@@ -1,12 +1,29 @@
 import * as React from "react";
+import { useRef, useState } from "react";
 import { generate, kDefaultRandom } from "../preset";
 import { CharacterComponent } from "./character";
-import { useState } from "react";
+import XXH from "xxhashjs";
+import MersenneTwister from "mersenne-twister";
+import { Character } from "../types";
 
 export const Main: React.FC = () => {
   const [count, setCount] = useState<number>(0);
   const update = () => {
     setCount(count + 1);
+  };
+  const ref = useRef<HTMLInputElement>(null);
+  const [character, setCharacter] = useState<Character | null>(null);
+  const onSubmit = () => {
+    const element = ref.current;
+    if (!element) {
+      return;
+    }
+    const name = element.value;
+    const hash = XXH.h64();
+    const day = Math.floor(Date.now() / (24 * 60 * 60 * 1000));
+    const seed = hash.init(0).update(name).update(`${day}`).digest();
+    const mt = new MersenneTwister(seed.toNumber());
+    setCharacter(generate({ gen: () => mt.random() }));
   };
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
@@ -45,6 +62,11 @@ export const Main: React.FC = () => {
       </div>
       <div style={{ userSelect: "none", cursor: "pointer" }} onClick={update}>
         Next
+      </div>
+      <div>
+        <input type={"text"} ref={ref} />
+        <input type={"button"} onClick={onSubmit} />
+        {character && <CharacterComponent character={character} size={20} />}
       </div>
     </div>
   );
