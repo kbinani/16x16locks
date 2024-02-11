bffb9374e963a3d609597751142b30e4968f9b23
kbinani <kbinani.bt@gmail.com>
2024-02-11 15:41:29 +0900

    Add next button

--- src/component/main.tsx
+++ src/component/main.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
 import { generate, kDefaultRandom } from "../preset";
 import { CharacterComponent } from "./character";
+import { useState } from "react";
 
 export const Main: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
+  const update = () => {
+    setCount(count + 1);
+  };
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <div style={{ display: "flex" }}>
@@ -37,6 +42,9 @@ export const Main: React.FC = () => {
         <div style={{ margin: 10 }}>
           <CharacterComponent character={generate(kDefaultRandom)} size={20} />
         </div>
+      </div>
+      <div style={{ userSelect: "none", cursor: "pointer" }} onClick={update}>
+        Next
       </div>
     </div>
   );
