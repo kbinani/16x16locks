4dd4cb2b4d764320433be1755a347b9667a9de04
kbinani <kbinani.bt@gmail.com>
2024-02-11 13:44:09 +0900

    Fix build script

--- dev.ts
+++ dev.ts
@@ -3,10 +3,9 @@ import * as path from "path";
 
 (async () => {
   const ctx = await esbuild.context({
-    define: { JE2BE_VERSION: '"(local)"' },
     entryPoints: [path.join(__dirname, "src/component/index.tsx")],
     bundle: true,
-    outfile: path.join(__dirname, "public", "script", "front.js"),
+    outfile: path.join(__dirname, "public", "script", "bundle.js"),
   });
   const { host, port } = await ctx.serve({
     servedir: path.join(__dirname, "public"),
